d8b6f23db81757d25519331b9b630ad0a2019d41
abdulmizwar <abdulmizwar@gmail.com>
2024-05-25 11:40:04 +0700

    coba

--- stack.cpp
+++ stack.cpp
@@ -33,7 +33,7 @@ void TampilData(){
     if (!isEmpty())
     {
 
-        cout<<"Data Tersimpan "<<endl;
+        cout<<"Data Tersimpan uh"<<endl;
         for (int i = 0; i < top; i++)
         {
             cout<< i + 1<<". "<<dataBuku[i]<<endl;
